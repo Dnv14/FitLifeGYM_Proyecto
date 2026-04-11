e74cab2529919dc6b9231f11f0a3458f0e6231f9
Dnv14 <diego.navarro.140706@gmail.com>
2026-04-10 18:27:13 -0700

    inicio
    inicio

---
+++ FitLifeGYM_Presentacion/src/main/java/com/mycompany/fitlifegym_presentacion/FitLifeGYM_Presentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fitlifegym_presentacion;
+
+/**
+ *
+ * @author Diego
+ */
+public class FitLifeGYM_Presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
