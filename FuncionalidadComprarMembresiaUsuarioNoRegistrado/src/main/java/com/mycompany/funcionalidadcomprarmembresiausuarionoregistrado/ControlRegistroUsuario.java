9312f556f2039febf4c8e503a75eaec3f0fc4a9b
Dnv14 <diego.navarro.140706@gmail.com>
2026-04-19 17:30:11 -0700

    control prueba
    control

---
+++ FuncionalidadComprarMembresiaUsuarioNoRegistrado/src/main/java/com/mycompany/funcionalidadcomprarmembresiausuarionoregistrado/ControlRegistroUsuario.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.funcionalidadcomprarmembresiausuarionoregistrado;
+
+import com.mycompany.funcionalidadcomprarmembresiausuarionoregistrado.dtos.NuevoClienteDTO;
+import com.mycompany.funcionalidadcomprarmembresiausuarionoregistrado.negocio.IClientesBO;
+
+/**
+ *
+ * @author Diego
+ */
+public class ControlRegistroUsuario implements FuncionalidadRegistrarUsuario {
+
+    private final IClientesBO clientesBO;
+
+    public ControlRegistroUsuario(IClientesBO clientesBO) {
+        this.clientesBO = clientesBO;
+    }
+
+    @Override
+    public void RegistrarUsuario(NuevoClienteDTO clienteDTO) {
+        clientesBO.registrarCliente(clienteDTO);
+    }
+}
